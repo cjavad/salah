13acf7a1b7d604b540886d59fd84765ecec99f77
Javad Shafique <javadshafique@hotmail.com>
2018-03-02 12:31:18 +0100

    fixed some issues

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,11 +1,11 @@
-function gettimes(lat, lng, method, callback) {
+function gettimes(lat, lng, method, callback, base_url = "") {
     var date = new Date();
     var timezone = -(date.getTimezoneOffset() / 60)
     $.ajax({
         type: "GET",
-        url: "/api/" + lat + "/" + lng + "/" + timezone + "/",
+        url: base_url + "/api/" + lat + "/" + lng + "/" + timezone + "/",
         data: {
-            method: method || MWL,
+            method: method || "MWL",
             date: date
         },
         success: callback
